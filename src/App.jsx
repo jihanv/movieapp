20890818063ac169d5136b94f6d5de30d4c8e16c
jihanv <jihanv@gmail.com>
2025-05-29 08:38:32 +0900

    Add state to button

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-// import { useState } from 'react'
+import { useState } from "react";
 
 import "./index.css";
 
 const Card = (props) => {
+  const [hasLiked, setHasLiked] = useState(false);
   return (
-    <div style={{
-      border: '1px solide "#4b5362"',
-      padding: '20px',
-      margin: '10px',
-      backgroundClip: "#31363f",
-      borderRadius: "10px",
-      minHeight: "100px"
-    }}>
+    <div className="card">
       <h2>{props.title}</h2>
+      <button onClick={(prev) => setHasLiked(!prev.hasLiked)}>{hasLiked? "Liked" : "Like"}</button>
     </div>
   );
 };
 const App = () => {
   return (
     <div className="card-container">
-      <Card title="Star Wars"/>
-      <Card title="The Lion King"/>
-      <Card title="Avatar"/>
+      <Card title="Star Wars" />
+      <Card title="The Lion King" />
+      <Card title="Avatar" />
     </div>
-  )
+  );
 };
 
 export default App;
