1937e799e9c4f218b436c94b6710e8a1784f3b69
jihanv <jihanv@gmail.com>
2025-05-28 22:13:45 +0900

    Add css styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import "./index.css";
 
 const Card = (props) => {
   return (
-    <div>
+    <div style={{
+      border: '1px solide "#4b5362"',
+      padding: '20px',
+      margin: '10px',
+      backgroundClip: "#31363f",
+      borderRadius: "10px",
+      minHeight: "100px"
+    }}>
       <h2>{props.title}</h2>
     </div>
   );
 };
 const App = () => {
   return (
-    <>
-      <h2>New Content</h2>
+    <div className="card-container">
       <Card title="Star Wars"/>
       <Card title="The Lion King"/>
       <Card title="Avatar"/>
-    </>
-  );
+    </div>
+  )
 };
 
 export default App;
