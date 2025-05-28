f83960e2b390936d95aa818a2b8b946bac49eb00
jihanv <jihanv@gmail.com>
2025-05-28 22:06:52 +0900

    Remove more vite default

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 // import { useState } from 'react'
 
-import './App.css'
+import "./index.css";
 
+const Card = (props) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+    </div>
+  );
+};
 const App = () => {
   return (
-    <h2>New Content</h2>
-  )
-}
+    <>
+      <h2>New Content</h2>
+      <Card title="Star Wars"/>
+      <Card title="The Lion King"/>
+      <Card title="Avatar"/>
+    </>
+  );
+};
 
-export default App
+export default App;
