93e8b75fb3a4ee10827f1eb091f876b8a00fe270
jihanv <jihanv@gmail.com>
2025-05-29 10:12:41 +0900

    Add assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span></h1>
+        </header>
+        <p>Search</p>
+      </div>
     </div>
   )
 }
