5a4d3211e7b7873c4309e82bb321e8a5cd1b1cdd
jihanv <jihanv@gmail.com>
2025-05-29 09:59:25 +0900

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useState, useEffect } from "react";
+import React from 'react'
 
-import "./index.css";
-
-const Card = (props) => {
-  const [count, setCount] = useState(0)
-  const [hasLiked, setHasLiked] = useState(false);
-
-  function addCount(){
-    setCount((prev)=> prev+1)
-  }
-  useEffect(() => console.log(`${props.title} has been liked.`), [hasLiked])
-  return (
-    <div className="card" onClick={addCount}>
-      <h2>{props.title} - <br/>{count>0? count: ""}</h2>
-      <button onClick={() => setHasLiked((prev)=> !prev)}>{hasLiked? "Liked" : "Like"}</button>
-    </div>
-  );
-};
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Star Wars" />
-      <Card title="The Lion King" />
-      <Card title="Avatar" />
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello World!</h1>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
