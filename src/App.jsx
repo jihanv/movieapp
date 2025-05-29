17ed509090ee26d943ef8535dc95581b93a35f5b
jihanv <jihanv@gmail.com>
2025-05-29 10:37:45 +0900

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Search from './components/Search'
 
 const App = () => {
   return (
@@ -6,9 +7,10 @@ const App = () => {
       <div className='pattern'/>
       <div className='wrapper'>
         <header>
+          <img src="/movieapp/hero.png" alt="Hero Banner" />
           <h1>Find <span className='text-gradient'>Movies</span></h1>
         </header>
-        <p>Search</p>
+        <Search />
       </div>
     </div>
   )
