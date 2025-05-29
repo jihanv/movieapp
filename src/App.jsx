55900cf7e09fb60b0a0c1e88e94a121f23080c39
jihanv <jihanv@gmail.com>
2025-05-29 09:07:26 +0900

    Add count

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./index.css";
 
 const Card = (props) => {
+  const [count, setCount] = useState(0)
   const [hasLiked, setHasLiked] = useState(false);
+
+  function addCount(){
+    setCount((prev)=> prev+1)
+  }
+  useEffect(() => console.log(`${props.title} has been liked.`), [hasLiked])
   return (
-    <div className="card">
-      <h2>{props.title}</h2>
-      <button onClick={(prev) => setHasLiked(!prev.hasLiked)}>{hasLiked? "Liked" : "Like"}</button>
+    <div className="card" onClick={addCount}>
+      <h2>{props.title} - <br/>{count>0? count: ""}</h2>
+      <button onClick={() => setHasLiked((prev)=> !prev)}>{hasLiked? "Liked" : "Like"}</button>
     </div>
   );
 };
